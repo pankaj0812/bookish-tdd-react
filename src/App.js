ab327456b6deef8858f7137d5cf9ce362dcef75a
pankaj0812 <singhpankajkumar65@gmail.com>
2020-02-23 17:48:55 -0800

    Makes the first e2e test pass using cypress

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+
+function App() {
+  return (
+    <div>
+      <Typography variant="h2" component="h2" data-test="heading">
+        Bookish
+      </Typography>
+    </div>
+  );
+}
+
+export default App;
